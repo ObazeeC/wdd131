97fbdc4e0b4e1097427ce5bf51f72aef9d0aaf3b
obazeec <obazee.esosa@yahoo.com>
2024-11-09 21:06:26 +0100

    test responsiveness week 02

--- scripts/temple.js
+++ scripts/temple.js
@@ -5,3 +5,15 @@ document.getElementById("currentyear").innerHTML = year;
 
 let modify = document.lastModified;
 document.getElementById("lastModified").innerHTML = modify;
+
+// store the selected element that we need in variables
+const mainnav = document.querySelector('.navigation');
+const tempbutton = document.querySelector('#menu');
+
+// Add  click event listener to the button 
+// Add a click event listener to the main menu (mainnav)
+
+tempbutton.addEventListener('click', () => {
+mainnav .classList.toggle('show');
+tempbutton.classList.toggle('show');
+});
