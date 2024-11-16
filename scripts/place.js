0bf5eff627db482c9f5ac98a2e9722e686d81ecd
obazeec <obazee.esosa@yahoo.com>
2024-11-16 18:40:21 +0100

    cell

--- scripts/place.js
+++ scripts/place.js
@@ -11,3 +11,38 @@ document.querySelector("#currentyear");
 
 let modify = document.lastModified;
 document.getElementById("lastModified").innerHTML = modify;
+
+const temp = 34;
+const wind = 6.6;
+
+function calculateWindChill(temp, wind){
+    if (temp <= 10 && wind >=4.8){
+       const  windhill = 13.12 + 0.6215 * temp +(0.3965 * temp - 11.37) * Math.pow(wind, 0.16);
+         return windhill.toFixed(2);
+    }
+  // return  35.74 + 0.6215 * temp*(0.4275 * temp - 35.75)* wind**0.16;
+    
+}
+
+const weather = document.querySelector('#weather');
+weather.textContent = calculateWindChill(3, 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
