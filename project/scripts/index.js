5fb7b22005822186af58368f5bf34ff285db6d54
obazeec <obazee.esosa@yahoo.com>
2024-12-12 22:15:04 +0000

    navigation menu small

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -1,14 +1,16 @@
-// Store the selected element that we are going to use
-const mainnav = document.querySelector('.navigation');
-const hambutton = document.querySelector('#menu');
+//NAVIGATION MENU
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
 
 // Add a click event listener to the hamburger button 
 // And use a callback function that toggles the list element's list of classes
-hambutton.addEventListener('click', () =>{
-mainnav.classList.toggle('show');
-hambutton.classList.toggle('show');
 
-});
 // What does toggle mean ? Instead of separate add and remove statements, toggle means add the class if it does not currently exist
 // or remove the named class if it does exist. 
 //The CSS class rules wil handle the different views, layouts, and displays. JavaScript only applies the class value or not. 
